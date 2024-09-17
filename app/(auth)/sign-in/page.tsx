3c20f9f3e9833da8c25be438ff4aeb22d73df79d
ptrwoo <144814534+ptrwoo@users.noreply.github.com>
2024-09-17 13:21:09 -0400

    restructured added commPage session components commented out old code

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
 import { getSessionName, signIn } from "@/utils/authService";
+import SignUp from "../sign-up/page";
+import { useRouter } from "next/router";
 
 interface SignInProps {
   onSignIn: (userId: string, name: string) => void;
@@ -16,6 +18,11 @@ const SignIn: React.FC<SignInProps> = ({ onSignIn }) => {
   };
 
   const sessionName = Object.values(getSessionName())[0];
+  const router = useRouter();
+
+  const handleNavigate = () => {
+    router.push("/sign-up");
+  };
 
   return (
     <div className="font-mono">
@@ -29,8 +36,52 @@ const SignIn: React.FC<SignInProps> = ({ onSignIn }) => {
       <button className="bg-orange-500" onClick={handleSignIn}>
         Sign In
       </button>
+
+      <p>dont have an account?</p>
+      <button onClick={handleNavigate}>Sign Up</button>
     </div>
   );
 };
 
 export default SignIn;
+
+// const SignIn: React.FC<SignInProps> = ({ onSignIn }) => {
+//   const [name, setName] = useState("");
+
+//   const handleSignIn = () => {
+//     if (!name) return;
+
+//     const { userId, name: savedName } = signIn(name);
+//     onSignIn(userId, savedName);
+//   };
+
+//   const sessionName = Object.values(getSessionName())[0];
+
+//   return (
+//     <div className="font-mono">
+//       <h1 className="text-xl">Sign In for {sessionName}</h1>
+//       <input
+//         type="text"
+//         placeholder="Enter your name"
+//         value={name}
+//         onChange={(e) => setName(e.target.value)}
+//       />
+//       <button className="bg-orange-500" onClick={handleSignIn}>
+//         Sign In
+//       </button>
+//     </div>
+//   );
+// };
+
+// export default SignIn;
+
+/* 
+
+Sign in must accomodate:
+1) api call to database of storing name/uuid and password (perhaps email instead of uuid)
+2) allow a new user to create an account, or for an existing one to log in
+
+
+
+
+*/
---
+++ components/community/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Community = () => {
+  //signIn -> homepage (communities) -> inside community = sessions
+
+  return <div>Community</div>;
+};
+
+export default Community;
